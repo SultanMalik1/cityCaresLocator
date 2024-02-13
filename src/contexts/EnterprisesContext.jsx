ab6cdc658dc7167ba9010a3f81956c7806ef1b41
sultanmalik1 <sullionik@gmail.com>
2024-02-12 20:37:27 -0500

    fixing the search option

--- src/contexts/EnterprisesContext.jsx
+++ src/contexts/EnterprisesContext.jsx
@@ -50,62 +50,13 @@ function CitiesProvider({ children }) {
     }
   }
 
-  // async function createCity(newCity) {
-  //   try {
-  //     setIsLoading(true);
-
-  //     // Use Supabase API to insert data into the "organizations" table
-  //     const { data, error } = await supabase
-  //       .from("organizations")
-  //       .upsert([newCity]);
-
-  //     if (error) {
-  //       throw new Error("Error creating city in Supabase");
-  //     }
-
-  //     // Update the local state with the new data
-  //     setEnterprises((enterprises) => [...enterprises, data[0]]);
-  //   } catch (error) {
-  //     console.error("Error creating city:", error.message);
-  //     alert("There was an error creating city");
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }
-
-  // async function deleteCity(id) {
-  //   try {
-  //     setIsLoading(true);
-
-  //     // Use Supabase API to delete a record from the "organizations" table
-  //     const { data, error } = await supabase
-  //       .from("organizations")
-  //       .delete()
-  //       .eq("id", id);
-
-  //     if (error) {
-  //       throw new Error("Error deleting city in Supabase");
-  //     }
-
-  //     // Update the local state by filtering out the deleted city
-  //     setEnterprises((enterprises) =>
-  //       enterprises.filter((city) => city.id !== id)
-  //     );
-  //   } catch (error) {
-  //     console.error("Error deleting city:", error.message);
-  //     alert("There was an error deleting city");
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }
-
   function filterEnterprises(searchInput) {
     return enterprises.filter(
-      (city) =>
-        city.cityName?.toLowerCase()?.includes(searchInput.toLowerCase()) ||
-        city.country?.toLowerCase()?.includes(searchInput.toLowerCase()) ||
-        (city.notes &&
-          city.notes.toLowerCase().includes(searchInput.toLowerCase()))
+      (item) =>
+        item.name?.toLowerCase()?.includes(searchInput.toLowerCase()) ||
+        item.onliner?.toLowerCase()?.includes(searchInput.toLowerCase()) ||
+        (item.notes &&
+          item.notes.toLowerCase().includes(searchInput.toLowerCase()))
     );
   }
 
@@ -116,8 +67,6 @@ function CitiesProvider({ children }) {
         isLoading,
         currentEnterprise,
         getEnterprise,
-        // createCity,
-        // deleteCity,
         filterEnterprises,
       }}
     >
