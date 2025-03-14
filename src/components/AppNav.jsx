388b54eb86d9aee9b89b912bd2068ac1c612cf99
sultanmalik1 <sullionik@gmail.com>
2025-03-13 23:14:31 -0400

    fix(AppNav): Update navigation label to "Services & Organizations"

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,16 +1,16 @@
-import { NavLink } from "react-router-dom";
-import styles from "./AppNav.module.css";
+import { NavLink } from "react-router-dom"
+import styles from "./AppNav.module.css"
 
 function AppNav() {
   return (
     <nav className={styles.nav}>
       <ul>
         <li>
-          <NavLink to="enterprises">Services & Bussinesses</NavLink>
+          <NavLink to="enterprises">Services & Orgnaizations</NavLink>
         </li>
       </ul>
     </nav>
-  );
+  )
 }
 
-export default AppNav;
+export default AppNav
