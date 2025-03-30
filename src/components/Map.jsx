d68554873ea81a143710f407465ce57280852dfc
sultanmalik1 <sullionik@gmail.com>
2025-03-30 00:06:51 -0400

    feat: implement modal feature with open/close behaviour and basic styling

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,46 +1,48 @@
-import { useNavigate } from "react-router-dom";
-import styles from "./Map.module.css";
-import { useEnterprises } from "../contexts/EnterprisesContext";
-import Button from "./Button";
+import { useNavigate } from "react-router-dom"
+import styles from "./Map.module.css"
+import { useEnterprises } from "../contexts/EnterprisesContext"
+import Button from "./Button"
 
 import {
   MapContainer,
   TileLayer,
   Marker,
-  Popup,
   useMapEvents,
-} from "react-leaflet";
-import { useEffect, useState } from "react";
-import { useMap } from "react-leaflet";
-import { useGeolocation } from "../hooks/useGeolocation";
-import { useUrlPosition } from "../hooks/useUrlPosition";
+  useMap,
+} from "react-leaflet"
+import { useEffect, useState } from "react"
+import { useGeolocation } from "../hooks/useGeolocation"
+import { useUrlPosition } from "../hooks/useUrlPosition"
 
 function Map({ isSidebarCollapsed }) {
-  const { enterprises } = useEnterprises();
+  // Fetch all organizations from supabase
+  const { enterprises } = useEnterprises()
+  // The organization selected by user (for displaying in modal)
+  const [selectedEnterprise, setSelectedEnterprise] = useState(null)
+
+  // Default location is centered on Manhattan
   const [mapPosition, setMapPosition] = useState([
     40.77206305312022, -73.9627399862185,
-  ]);
+  ])
+  // Custom hook for geolocation
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
-  } = useGeolocation();
-  const [mapLat, mapLng] = useUrlPosition();
+  } = useGeolocation()
 
-  useEffect(
-    function () {
-      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
-    },
-    [mapLat, mapLng]
-  );
+  // Get the location from url parameters
+  const [mapLat, mapLng] = useUrlPosition()
 
-  useEffect(
-    function () {
-      if (geolocationPosition)
-        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
-    },
-    [geolocationPosition]
-  );
+  // Update the map if the lat/lng changes
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
+  }, [mapLat, mapLng])
+
+  useEffect(() => {
+    if (geolocationPosition)
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+  }, [geolocationPosition])
 
   return (
     <div className={styles.mapContainer}>
@@ -49,9 +51,9 @@ function Map({ isSidebarCollapsed }) {
           {isLoadingPosition ? "Loading..." : "Use your position"}
         </Button>
       )}
+
       <MapContainer
         center={mapPosition}
-        // center={[mapLat, mapLng]}
         zoom={12}
         scrollWheelZoom={true}
         className={styles.map}
@@ -60,36 +62,58 @@ function Map({ isSidebarCollapsed }) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
+
         {enterprises.map((enterprise) => (
           <Marker
-            position={[enterprise.position.lat, enterprise.position.lng]}
             key={enterprise.id}
-          >
-            <Popup>
-              <span>{enterprise.name}</span>
-            </Popup>
-          </Marker>
+            position={[enterprise.position.lat, enterprise.position.lng]}
+            eventHandlers={{
+              click: () => setSelectedEnterprise(enterprise),
+            }}
+          />
         ))}
+
         <ChangeCenter position={mapPosition} />
         <DetectClick />
       </MapContainer>
+
+      {/*Modal*/}
+      {selectedEnterprise && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modalContent}>
+            <h2>{selectedEnterprise.name}</h2>
+            <p>{selectedEnterprise.oneliner}</p>
+            <div className={styles.buttonContainer}>
+              {selectedEnterprise.website && (
+                <a href={selectedEnterprise.website}>
+                  <button className={styles.websiteButton}>Visite Site</button>
+                </a>
+              )}
+              <button
+                onClick={() => setSelectedEnterprise(null)}
+                className={styles.closeButton}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 function ChangeCenter({ position }) {
-  const map = useMap();
-  map.setView(position);
-  return null;
+  const map = useMap()
+  map.setView(position)
+  return null
 }
 
 function DetectClick() {
-  const navigate = useNavigate();
-
+  const navigate = useNavigate()
   useMapEvents({
-    // this code below allows us to navigate to form when user clicks on anywhere on the map other than tag
     // click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
-  });
+  })
 }
 
-export default Map;
+export default Map
